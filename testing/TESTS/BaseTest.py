8a75cf5e16c529e178f636379186cbc1b91fa7a7
Austin <acs202204@gmail.com>
2025-12-04 10:33:53 -0500

    Delete testing directory to update

--- testing/TESTS/BaseTest.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("venv/testing/yolo-hand-pose-main/model/best.pt")  # nano model
-results = model("testImage.jpg")  # replace with your image
-results.show()
