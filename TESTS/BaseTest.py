186e1e46f4b4182f4ba86734d1d0593822e4b19b
Austin <acs202204@gmail.com>
2026-01-28 09:35:44 -0500

    Updating Files

--- TESTS/BaseTest.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("/home/team10sp/Desktop/individualWork/Austin/YOLO/yolo_project/testing/yolo-hand-pose-main/model/best.pt")  # nano model
-results = model("testImage.jpg")  # replace with your image
-results.show()
